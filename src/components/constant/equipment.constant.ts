7994f3e48e6b15883af5d0e98faecfe9468f9321
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-13 12:12:33 -0700

    set: array of possible equipment that can be currently in the vehicle

---
+++ src/components/constant/equipment.constant.ts
@@ -0,0 +1,12 @@
+export const POSSIBLE_EQUIPMENT = [
+    "Air Ride",
+    "Dock High",
+    "Dolly",
+    "E-Track",
+    "Hazardous Material",
+    "Lift Gate",
+    "Pallet Jack",
+    "Ramps",
+    "Side Door",
+    "Temperature Control",
+]
