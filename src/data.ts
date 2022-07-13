dcc68b5bb3e0f65a49514edcc6e200810ec96433
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-13 11:22:14 -0700

    update: moved dummy data to .ts file for better data reprenstation adjusted it to be an array of objects

---
+++ src/data.ts
@@ -0,0 +1,45 @@
+import { Equipment } from "./interface/equipment.interface";
+
+
+export const data: Equipment[] = [
+    {
+        "equipment_name": "Air Ride",
+        "is_included":     false
+    },
+    {
+        "equipment_name": "Dock High",
+        "is_included":     true
+    },
+    {
+        "equipment_name": "Dolly",
+        "is_included":     true
+    },
+    {
+        "equipment_name": "E-Track",
+        "is_included":     true
+    },
+    {
+        "equipment_name": "Hazardous Material",
+        "is_included":     true
+    },
+    {
+        "equipment_name": "Lift Gate",
+        "is_included":     false
+    },
+    {
+        "equipment_name": "Pallet Jack",
+        "is_included":     true
+    },
+    {
+        "equipment_name": "Ramps",
+        "is_included":     false
+    },
+    {
+        "equipment_name": "Side Door",
+        "is_included":     false
+    },
+    {
+        "equipment_name": "Temperature Control",
+        "is_included":     true
+    }
+]
